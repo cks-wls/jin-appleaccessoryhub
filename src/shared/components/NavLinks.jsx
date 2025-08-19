579ec4aeda58359a5534c5c567b722e7cc3c0a29
cks-wls <slo0521@naver.com>
2025-08-19 15:45:16 +0900

    feat : 반응형 작업을 위한 아이콘 추가

--- src/shared/components/NavLinks.jsx
+++ src/shared/components/NavLinks.jsx
@@ -1,6 +1,7 @@
 import likeIcon from "../assets/like.svg";
 import cartIcon from "../assets/cart.svg";
 import profileIcon from "../assets/profile.svg";
+import etcIcon from "../assets/etc.svg";
 import "./style/navLinksStyle.css";
 
 export default function NavLinks() {
@@ -9,6 +10,7 @@ export default function NavLinks() {
       <img src={likeIcon} alt="Like Icon" className="nav-icon" />
       <img src={cartIcon} alt="Cart Icon" className="nav-icon" />
       <img src={profileIcon} alt="Profile Icon" className="nav-icon" />
+      <img src={etcIcon} alt="etc Icon" className="etc-icon" />
     </div>
   );
 }
