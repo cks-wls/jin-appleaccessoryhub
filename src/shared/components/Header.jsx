b4589dca984152a3510d8acf3a6e3dded50d0c9c
cks-wls <slo0521@naver.com>
2025-08-19 15:45:02 +0900

    feat : 기존 제목대신 아이콘으로 추가

--- src/shared/components/Header.jsx
+++ src/shared/components/Header.jsx
@@ -1,11 +1,12 @@
 import "./style/headerStyle.css";
 import SearchBar from "./SearchBar";
 import NavLinks from "./NavLinks";
+import blackLogo from "../assets/blacklogo.svg";
 export default function Header() {
   return (
     <>
       <div className="container">
-        <h2>Cyber</h2>
+        <img src={blackLogo} alt="Apple Accessory Hub Logo" className="logo" />
         <SearchBar />
         <NavLinks />
       </div>
