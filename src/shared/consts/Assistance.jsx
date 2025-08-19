a2bdc775528be0efbf1ead18000eaf94695331b1
cks-wls <slo0521@naver.com>
2025-08-19 20:52:58 +0900

    feat : footer 컴포넌트 스타일링

---
+++ src/shared/consts/Assistance.jsx
@@ -0,0 +1,13 @@
+export default function Assistance() {
+  return (
+    <>
+      <ul>Assitance to the buyer</ul>
+      <li>Find an order</li>
+      <li>Terms of delivery</li>
+      <li>Exchabge and return of goods</li>
+      <li>Guarantee</li>
+      <li>Frequently asked questions</li>
+      <li>Terms of use of the site</li>
+    </>
+  );
+}
