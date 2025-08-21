0e740beac20cff4198f0b2e3d18ce41a5dd9f849
cks-wls <slo0521@naver.com>
2025-08-21 14:44:06 +0900

    chore : favicon 수정

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import favicon from "../app/assets/icons/cyber.png";
+
+const link = document.createElement("link");
+link.rel = "icon";
+link.href = favicon;
+document.head.appendChild(link);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
