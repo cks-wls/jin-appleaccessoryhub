6afa252a914ab1e21a4e72fd6a020e6680214465
cks-wls <slo0521@naver.com>
2025-09-02 11:42:35 +0900

    feat :  상품 컴포넌트 추가

--- src/app/pages/main.jsx
+++ src/app/pages/main.jsx
@@ -2,6 +2,7 @@ import AdProductContainer from "@/components/main/AdProductContainer";
 import BottomProductContainer from "@/components/main/BottomProductContainer";
 import BottomAdContainer from "@/components/main/BottomAdContainer";
 import CategoryContainer from "@/components/main/CategoryContainer";
+import ProductContainer from "@/components/main/ProductContainer";
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
       <AdProductContainer />
       <BottomProductContainer />
       <CategoryContainer />
+      <ProductContainer />
       <BottomAdContainer />
     </>
   );
