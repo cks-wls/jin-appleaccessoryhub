ae4f2e9f27d50c00acd20982dc91870c340a0134
cks-wls <slo0521@naver.com>
2025-09-12 20:29:55 +0900

    feat : 카테고리 스켈레톤 구현

---
+++ src/app/components/skeleton/CategorySkeleton.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+function CategorySkeleton() {
+  return <Container></Container>;
+}
+export default CategorySkeleton;
+const Container = styled.div`
+  width: 100%;
+  height: 20px;
+  background-color: lightgray;
+  border-radius: 10px;
+`;
